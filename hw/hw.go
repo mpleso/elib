38f7015082df4a1f0966fe9082ca8649e56cc5b3
Eliot Dresselhaus <edressel@platinasystems.com>
2015-11-25 09:13:38 -0800

    * moved to hw repo
    Signed-off-by: Eliot Dresselhaus <edressel@platinasystems.com>

--- hw/hw.go
+++
@@ -1,8 +0,0 @@
-// Memory mapped register read/write
-package hw
-
-// Memory-mapped read/write
-func LoadUint32(addr *uint32) (data uint32)
-func StoreUint32(addr *uint32, data uint32)
-
-func MemoryBarrier()
