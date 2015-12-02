297672965b584a57fae9006bb9a7ddf7845f8807
Eliot Dresselhaus <edressel@platinasystems.com>
2015-12-02 11:30:59 -0800

    add Word receiver forms of bit operations
    Signed-off-by: Eliot Dresselhaus <edressel@platinasystems.com>

--- bits.go
+++ bits.go
@@ -18,6 +18,7 @@ func NSetBits(y Word) uint {
 	x *= 0x0101010101010101
 	return uint(((x >> 56) & 0xFF))
 }
+func (x Word) NSetBits() uint { return NSetBits(x) }
 
 func NLeadingZeros(x Word) uint {
 	n := uint(WordBits)
@@ -56,25 +57,22 @@ func NLeadingZeros(x Word) uint {
 	}
 	return n - uint(x)
 }
+func (x Word) NLeadingZeros() uint { return NLeadingZeros(x) }
 
 // firstSet gives 2^f where f is the lowest 1 bit in x
-func FirstSet(x Word) Word {
-	return x & -x
-}
+func FirstSet(x Word) Word    { return x & -x }
+func (x Word) FirstSet() Word { return FirstSet(x) }
 
 // isPow2 true for x a power of 2
-func IsPow2(x Word) bool {
-	return 0 == x&(x-1)
-}
+func IsPow2(x Word) bool    { return 0 == x&(x-1) }
+func (x Word) IsPow2() bool { return IsPow2(x) }
 
 // roundPow2 rounds x to next power of two >= x
-func RoundPow2(x, p Word) Word {
-	return (x + p - 1) &^ (p - 1)
-}
+func RoundPow2(x, p Word) Word       { return (x + p - 1) &^ (p - 1) }
+func (x Word) RoundPow2(p Word) Word { return RoundPow2(x, p) }
 
-func MinLog2(x Word) uint {
-	return 63 - NLeadingZeros(x)
-}
+func MinLog2(x Word) uint    { return 63 - NLeadingZeros(x) }
+func (x Word) MinLog2() uint { return MinLog2(x) }
 
 func MaxLog2(x Word) uint {
 	l := MinLog2(x)
@@ -83,6 +81,7 @@ func MaxLog2(x Word) uint {
 	}
 	return l
 }
+func (x Word) MaxLog2() uint { return MaxLog2(x) }
 
 func MaxPow2(x Word) Word {
 	z := MinLog2(x)
@@ -92,6 +91,7 @@ func MaxPow2(x Word) Word {
 	}
 	return y
 }
+func (x Word) MaxPow2() Word { return MaxPow2(x) }
 
 func NextSet(x Word) (v Word, i int) {
 	f := x & -x
